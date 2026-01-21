57ac010514798cee0c3a3fb0bc517e086c40a9cf
Dinakar Cheruku <dinakarcheruku2002@gmail.com>
2026-01-21 14:56:12 -0500

    updated inspect_data.py file to view dtypes to prepare for db ingestion

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -1,20 +1,38 @@
-import csv
+import os
+import pandas as pd
 
-sales_file = '../data/sales_inventory_dataset.csv'
-student_file = "../data/student_information_dataset.csv"
+from src.build_fact_tables import fact_sales
 
-def inspect_csv(file_path, limit=5):
-    with open(file_path, newline="", encoding="utf-8") as f:
-        reader = csv.reader(f)
-        headers = next(reader)
-        print(f"\nFile: {file_path}")
-        print("Headers:", headers)
+raw_dir = "../data/uncleaned_dimensions"
+clean_dir = "../data/cleaned_dimensions"
+CLEAN_DIM_DIR = "../data/cleaned_dimensions"
+FACT_DIR = "../data/fact_tables"
 
-        print("\nSample Rows:")
-        for i, row in enumerate(reader):
-            if i >= limit:
-                break
-            print(row)
+# Load raw dimension tables
+# dim_category_raw = pd.read_csv(os.path.join(raw_dir, "dim_category_raw.csv"))
+# dim_product_raw = pd.read_csv(os.path.join(raw_dir, "dim_product_raw.csv"))
+# dim_supplier_raw = pd.read_csv(os.path.join(raw_dir, "dim_supplier_raw.csv"))
+# dim_date_sales_raw = pd.read_csv(os.path.join(raw_dir, "dim_date_sales_raw.csv"))
+# dim_sale_raw = pd.read_csv(os.path.join(raw_dir, "dim_sale_raw.csv"))
+#
+# dim_major_raw = pd.read_csv(os.path.join(raw_dir, "dim_major_raw.csv"))
+# dim_student_raw = pd.read_csv(os.path.join(raw_dir, "dim_student_raw.csv"))
+# dim_date_enrollment_raw = pd.read_csv(os.path.join(raw_dir, "dim_date_enrollment_raw.csv"))
 
-inspect_csv(sales_file)
-inspect_csv(student_file)
+
+dim_category_cleaned = pd.read_csv(f"{CLEAN_DIM_DIR}/dim_category_cleaned.csv")
+dim_date_sales_cleaned = pd.read_csv(f"{CLEAN_DIM_DIR}/dim_date_sales_cleaned.csv")
+dim_date_enrollment_cleaned = pd.read_csv(f"{CLEAN_DIM_DIR}/dim_date_enrollment_cleaned.csv")
+dim_product_cleaned = pd.read_csv(f"{CLEAN_DIM_DIR}/dim_product_cleaned.csv")
+dim_sale_cleaned = pd.read_csv(f"{CLEAN_DIM_DIR}/dim_sale_cleaned.csv")
+dim_supplier_cleaned = pd.read_csv(f"{CLEAN_DIM_DIR}/dim_supplier_cleaned.csv")
+
+dim_student_cleaned = pd.read_csv(f"{CLEAN_DIM_DIR}/dim_student_cleaned.csv")
+dim_major_cleaned = pd.read_csv(f"{CLEAN_DIM_DIR}/dim_major_cleaned.csv")
+
+fact_sales = pd.read_csv(f"{FACT_DIR}/fact_sales.csv")
+fact_student = pd.read_csv(f"{FACT_DIR}/fact_student.csv")
+print(fact_sales.info())
+print(fact_student.info())
+print(fact_sales.head())
+print(fact_student.head())
